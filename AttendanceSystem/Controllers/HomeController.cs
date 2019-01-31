0b20d2106c1275d70b4026a6cb89549d1170a47b
Prajwol Adhikari <prajwolad18@gmail.com>
2019-01-31 18:41:23 +0545

    Add project files.

---
+++ AttendanceSystem/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AttendanceSystem.Models;
+
+namespace AttendanceSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Clock()
+        {
+            ViewData["Message"] = "Add one or more employees by entering their information below. All fields are mandatory";
+
+            return View();
+        }
+
+        public IActionResult Employees()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
